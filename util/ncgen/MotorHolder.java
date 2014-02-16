56b84ec394cc4b7005adb815d3a937a75f7b9100
Peter Andersson <pelleplutt1976@gmail.com>
2014-02-16 20:41:23 +0100

    modified a bit

--- util/ncgen/MotorHolder.java
+++ util/ncgen/MotorHolder.java
@@ -1,18 +1,21 @@
 import java.io.PrintStream;
 
-
 public class MotorHolder {
 
-  static double workPieceDepth;
-  static double routerDia;
-  static double motorWidth = 12+1.5;
-  static double motorLength= 9+15+1.5;
+  static double workPieceDepth = 11.5;
+  static double routerDia = 2.8;
+  static double motorWidth = 12+0.5;
+  static double motorLength= 9+15+0.5;
   static double motorDepth = 10;
-  static double flange = 10;
-  static double wall = 2;
+  static double flange = 5;
+  static double wall = 1.5;
+  
   static double feed = 150;
   static double zStep = 1;
   static double routeOverlap = 0.5;
+  static double hoverDistance = 1.0;
+  static double extraToBottom = 0.8;
+  
   static PrintStream out = System.out;
 
   // motor
@@ -44,7 +47,7 @@ public class MotorHolder {
     if (args.length < 2) {
       out.println("(no depth or router diameter set)");
       out.println("(usage: java MotorHolder <work piece depth in mm> <router diameter in mm>)");
-      workPieceDepth = 13;
+      workPieceDepth = 11.5;
       routerDia = 2.8;
       out.println("(assuming depth and diameter)");
     } else {
@@ -67,11 +70,7 @@ public class MotorHolder {
         + (motorDepth + wall) + ")");
   
     out.println();
-    out.println("(header)");
-    out.println("G21");
-    out.println("G90");
-    out.println("G00 Z1");
-    out.println("G00 X0 Y0");
+    start();
     
     out.println();
     out.println("(top shave)");
@@ -107,7 +106,7 @@ public class MotorHolder {
         flange - wall - routerDia, 
         motorLength / 2, 
         workPieceDepth - (motorDepth + wall),
-        workPieceDepth + 0.7);
+        workPieceDepth + extraToBottom);
     
     out.println();
     out.println("(motor non flange right)");
@@ -116,28 +115,70 @@ public class MotorHolder {
         flange - wall - routerDia, 
         motorLength / 2, 
         workPieceDepth - (motorDepth + wall),
-        workPieceDepth + 0.7);
+        workPieceDepth + extraToBottom);
 
     out.println();
     out.println("(cutout)");
-    cutout(0,0,
-        motorWidth + flange * 2 + routerDia, 
-        motorLength + wall * 2 + routerDia, 
+    routeCutout(0,0,
+        motorWidth + flange * 2 + routerDia - 0.5, 
+        motorLength + wall * 2 + routerDia - 0.5, 
         0,
-        workPieceDepth + 0.7);
+        workPieceDepth + extraToBottom);
     
-    out.println("(end)");
-    out.println("G01 Z1");
-    out.println("G00 X0 Y0");
-    out.println("M05");
-    out.println("M02");
+    end();
   }
   
-  // route a cutout from top to given z
-  // does not take route diameter in account
-  static void cutout(double cx, double cy, double cw, double ch, double startZ, double z) {
-    if (startZ == z) return;
-    System.out.println(
+  // route a groove defined from (x1,y1) to (x2,y2) from start z to given z
+  static void routeGroove(double x1, double y1, double x2, double y2, double startZ, double z) {
+    if (startZ >= z) return;
+    out.println(
+        "(groove: " + x1 + "," + y1 + 
+        " to " + x2 + "," + y2 + 
+        ", from -" + startZ + " to -" + z + ")");
+    
+    double curZ = startZ + zStep;
+    boolean stop = false;
+    boolean dir = true;
+
+    goUp();
+    goXYRapid(x1, y1);
+    
+    do {
+      goZ(curZ);
+      if (dir) {
+        goXY(x1,y1);
+        goXY(x2,y2);
+      } else {
+        goXY(x2,y2);
+        goXY(x1,y1);
+      }
+      
+      dir = !dir;
+      
+      if (curZ == z) {
+        break;
+      } else if (curZ + zStep > z) {
+        curZ = z;
+        if (stop) {
+          break;
+        } else {
+          stop = true;
+        }
+      } else {
+        curZ += zStep;
+      }
+    } while (true);
+
+    goUp();
+  }
+  
+  // route a cutout from start z to given z
+  static void routeCutout(double cx, double cy, double cw, double ch, double startZ, double z) {
+    if (startZ >= z) return;
+    if (cw <= 0 || ch <= 0) {
+      throw new RuntimeException("Invalid dimension (" + cw + "x" + ch + ")");
+    }
+    out.println(
         "(cutout: middle " + cx + "," + cy + 
         " dim:" + cw + "x" + ch + 
         " from -" + startZ + " to -" + z + ")");
@@ -145,15 +186,15 @@ public class MotorHolder {
     double curZ = startZ + zStep;
     boolean stop = false;
 
-    out.println("G00 Z1");
-    out.println("G00 X" + (cx - cw/2) + " Y" +(cy - ch/2));
+    goUp();
+    goXYRapid(cx-cw/2, cy-ch/2);
     
     do {
-      out.println("G01 Z" + (-curZ) + " F" + feed);
-      out.println("G01 X" + (cx + cw/2) + " Y" +(cy - ch/2));
-      out.println("G01 X" + (cx + cw/2) + " Y" +(cy + ch/2));
-      out.println("G01 X" + (cx - cw/2) + " Y" +(cy + ch/2));
-      out.println("G01 X" + (cx - cw/2) + " Y" +(cy - ch/2));
+      goZ(curZ);
+      goXY(cx+cw/2, cy-ch/2);
+      goXY(cx+cw/2, cy+ch/2);
+      goXY(cx-cw/2, cy+ch/2);
+      goXY(cx-cw/2, cy-ch/2);
       
       if (curZ == z) {
         break;
@@ -169,28 +210,34 @@ public class MotorHolder {
       }
     } while (true);
 
-    out.println("G00 Z1");
+    goUp();
 
   }
   
-  // route out a box from top to given z
-  // does not take route diameter in account
+  // route out a box from start z to given z
   static void routeBox(double cx, double cy, double cw, double ch, double startZ, double z) {
-    if (startZ == z) return;
-    System.out.println(
+    if (startZ >= z) return;
+    if (cw <= 0 || ch <= 0) {
+      throw new RuntimeException("Invalid dimension (" + cw + "x" + ch + ")");
+    }
+
+    out.println(
         "(box: middle " + cx + "," + cy + 
         " dim:" + cw + "x" + ch + 
         " from -" + startZ + " to -" + z + ")");
     double curZ = startZ + zStep;
     boolean stop = false;
+    boolean planeDir = false;
     
-    out.println("G00 Z1");
-    out.println("G00 X" + (cx - cw/2) + " Y" +(cy - ch/2));
+    goUp();
+    goXYRapid(cx-cw/2, cy-ch/2);
     do {
-      out.println("G01 Z" + (-curZ) + " F" + feed);
-      routePlane(cx, cy, cw, ch);
-      out.println("G01 Z" + (-(curZ-1)) + " F" + feed);
-      out.println("G00 X" + (cx - cw/2) + " Y" +(cy - ch/2));
+      goZ(curZ);
+      routePlane(cx, cy, cw, ch, planeDir);
+      goZ(curZ-hoverDistance);
+      goXYRapid(cx-cw/2, cy-ch/2);
+      
+      planeDir = !planeDir;
       
       if (curZ == z) {
         break;
@@ -205,42 +252,119 @@ public class MotorHolder {
         curZ += zStep;
       }
     } while (true);
-
-    out.println("G00 Z1");
+    
+    
+    // finally route deep outer rectangle
+    goZ(z);
+    goXY(cx-cw/2, cy-ch/2);
+    goXY(cx-cw/2, cy+ch/2);
+    goXY(cx+cw/2, cy+ch/2);
+    goXY(cx+cw/2, cy-ch/2);
+    goXY(cx-cw/2, cy-ch/2);
+    
+    // up
+    goUp();
   }
   
-  // route out a plane starting at cx-cw/2,cy-cy/2
+  // route out a plane starting at cx-cw/2,cy-ch/2
   // assuming z to be positioned, feed to be set, starting position to be set
-  // does not take route diameter in account
-  static void routePlane(double cx, double cy, double cw, double ch) {
-    double routeStep = routerDia - routeOverlap;
+  static void routePlane(double cx, double cy, double cw, double ch, boolean horiz) {
+    if (cw <= 0 || ch <= 0) {
+      throw new RuntimeException("Invalid dimension (" + cw + "x" + ch + ")");
+    }
 
-    double y = cy-ch/2;
-    boolean dir = true;
-    boolean stop = false;
-    do {
-      if (dir) {
-        out.println("G01 X" + (cx - cw/2) + " Y" + y);
-        out.println("G01 X" + (cx + cw/2) + " Y" + y);
-      } else {
-        out.println("G01 X" + (cx + cw/2) + " Y" + y);
-        out.println("G01 X" + (cx - cw/2) + " Y" + y);
-      }
-      dir = !dir;
-      
-      if (y == cy+ch/2) {
-        break;
-      } else if (y + routeStep > cy+ch/2) {
-        y = cy+ch/2;
-        if (stop) {
-          break;
+    double routeStep = routerDia - routeOverlap;
+    if (horiz) {
+  
+      double y = cy-ch/2;
+      boolean dir = true;
+      boolean stop = false;
+      do {
+        if (dir) {
+          goXY(cx-cw/2, y);
+          goXY(cx+cw/2, y);
         } else {
-          stop = true;
+          goXY(cx+cw/2, y);
+          goXY(cx-cw/2, y);
         }
-      } else {
-        y += routeStep;
-      }
-    } while (true);
+        dir = !dir;
+        
+        if (y == cy+ch/2) {
+          break;
+        } else if (y + routeStep > cy+ch/2) {
+          y = cy+ch/2;
+          if (stop) {
+            break;
+          } else {
+            stop = true;
+          }
+        } else {
+          y += routeStep;
+        }
+      } while (true);
+      
+    } else {
+      
+      double x = cx-cw/2;
+      boolean dir = true;
+      boolean stop = false;
+      do {
+        if (dir) {
+          goXY(x, cy-ch/2);
+          goXY(x, cy+ch/2);
+        } else {
+          goXY(x, cy+ch/2);
+          goXY(x, cy-ch/2);
+        }
+        dir = !dir;
+        
+        if (x == cx+cw/2) {
+          break;
+        } else if (x + routeStep > cx+cw/2) {
+          x = cx+cw/2;
+          if (stop) {
+            break;
+          } else {
+            stop = true;
+          }
+        } else {
+          x += routeStep;
+        }
+      } while (true);
+      
+    }
   }
   
+  static void goXY(double x, double y) {
+    out.println("G01 X" + x + " Y" + y);
+  }
+
+  static void goZ(double z) {
+    out.println("G01 Z" + (-z) + " F" + feed);
+  }
+  
+  static void goUp() {
+    out.println("G00 Z" + hoverDistance);
+  }
+
+  static void goXYRapid(double x, double y) {
+    out.println("G00 Z" + hoverDistance);
+    out.println("G00 X" + x + " Y" + y);
+  }
+  
+  static void start() {
+    out.println("(header)");
+    out.println("G21");
+    out.println("G90");
+    out.println("G00 Z"+ hoverDistance);
+    out.println("G00 X0 Y0");
+  }
+
+  static void end() {
+    out.println("(end)");
+    out.println("G01 Z" + hoverDistance);
+    out.println("G00 X0 Y0");
+    out.println("M05");
+    out.println("M02");
+  }
 }
