71b44f932e74234dc8b7d7efbce0a5f7e994a50d
Peter Andersson <pelleplutt1976@gmail.com>
2014-02-25 21:45:18 +0100

    new motorholder cnc file

--- util/ncgen/MotorHolder.java
+++ util/ncgen/MotorHolder.java
@@ -1,17 +1,20 @@
+import java.awt.geom.Point2D;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class MotorHolder {
 
   static double workPieceDepth = 11.5;
   static double routerDia = 2.8;
-  static double motorWidth = 12+0.5;
-  static double motorLength= 9+15+0.5;
+  static double motorWidth = 12+1.0;
+  static double motorLength= 9+15+1.0;
   static double motorDepth = 10;
-  static double flange = 5;
+  static double flangeWidth = 7;
   static double wall = 1.5;
+  static double flangePos = 4;
   
-  static double feed = 150;
-  static double zStep = 1;
+  static double feed = 180;
+  static double zStep = 1.5;
   static double routeOverlap = 0.5;
   static double hoverDistance = 1.0;
   static double extraToBottom = 0.8;
@@ -60,12 +63,12 @@ public class MotorHolder {
     out.println("(motor depth:      " + motorDepth + ")");
     out.println("(motor width:      " + motorWidth + ")");
     out.println("(motor length:     " + motorLength + ")");
-    out.println("(flange:           " + flange + ")");
+    out.println("(flange:           " + flangeWidth + ")");
     out.println("(wall:             " + wall + ")");
     out.println("(feed:             " + feed + ")");
     out.println("(z step:           " + zStep + ")");
     out.println("(final dimension:  "
-        + (motorWidth + flange*2) + "x"
+        + (motorWidth + flangeWidth*2) + "x"
         + (motorLength + wall*2) + "x" +
         + (motorDepth + wall) + ")");
   
@@ -76,7 +79,7 @@ public class MotorHolder {
     out.println("(top shave)");
     routeBox(
         0, 0,
-        motorWidth + flange * 2 + routerDia, 
+        motorWidth + flangeWidth * 2 + routerDia, 
         motorLength + wall * 2 + routerDia, 
         0,
         workPieceDepth - (motorDepth + wall));
@@ -88,7 +91,7 @@ public class MotorHolder {
         motorWidth - routerDia, 
         motorLength + wall * 2 - routerDia, 
         workPieceDepth - (motorDepth + wall),
-        workPieceDepth - (motorDepth + wall) + motorDepth - wall);
+        workPieceDepth - (motorDepth + wall) + motorDepth - wall*2);
     
     out.println();
     out.println("(motor fit bottom)");
@@ -100,32 +103,76 @@ public class MotorHolder {
         workPieceDepth - (motorDepth + wall) + motorDepth);
     
     out.println();
-    out.println("(motor non flange left)");
-    routeBox(
-        -(wall/2 + (motorWidth + flange) / 2), motorLength / 4 + wall/2,
-        flange - wall - routerDia, 
-        motorLength / 2, 
-        workPieceDepth - (motorDepth + wall),
-        workPieceDepth + extraToBottom);
-    
-    out.println();
-    out.println("(motor non flange right)");
-    routeBox(
-        +(wall/2 + (motorWidth + flange) / 2), motorLength / 4 + wall/2,
-        flange - wall - routerDia, 
-        motorLength / 2, 
-        workPieceDepth - (motorDepth + wall),
-        workPieceDepth + extraToBottom);
-
-    out.println();
     out.println("(cutout)");
-    routeCutout(0,0,
-        motorWidth + flange * 2 + routerDia - 0.5, 
-        motorLength + wall * 2 + routerDia - 0.5, 
-        0,
-        workPieceDepth + extraToBottom);
+    ArrayList<Coord> cutout = createPath();
+    double cow = motorWidth + flangeWidth * 2 + routerDia - 0.5;
+    double coh = motorLength + wall * 2 + routerDia - 0.5;
+    double coiw = motorWidth + wall * 2 + routerDia;
+    addToPath(cutout, -coiw/2, -coh/2);
+    
+    addToPath(cutout, coiw/2, -coh/2);
+    addToPath(cutout, coiw/2, -flangePos);
+    addToPath(cutout, cow/2, -flangePos);
+    addToPath(cutout, cow/2, coh/2 - flangePos);
+    addToPath(cutout, coiw/2, coh/2 - flangePos);
+    addToPath(cutout, coiw/2, coh/2);
+
+    addToPath(cutout, -coiw/2, coh/2);
+    addToPath(cutout, -coiw/2, coh/2 - flangePos);
+    addToPath(cutout, -cow/2, coh/2 - flangePos);
+    addToPath(cutout, -cow/2, -flangePos);
+    addToPath(cutout, -coiw/2, -flangePos);
+    //addToPath(cutout, -coiw/2, -coh/2);
+    
+    routePath(cutout, 0, workPieceDepth + extraToBottom);
+    
+    
     
     end();
+  }
+  
+  static ArrayList<Coord> createPath() {
+    return new ArrayList<Coord>();
+  }
+  
+  static ArrayList<Coord> addToPath(ArrayList<Coord> path, double x, double y) {
+    path.add(new Coord(x,y));
+    return path;
+  }
+  
+  static void routePath(ArrayList<Coord> path, double startZ, double z) {
+    if (startZ >= z) return;
+    if (path.isEmpty()) return;
+    out.println("(path)");
+    double curZ = startZ + zStep;
+    boolean stop = false;
+    boolean dir = true;
+
+    goUp();
+    goXYRapid(path.get(0).x, path.get(0).y);
+    
+    do {
+      goZ(curZ);
+      for (Coord coord : path) {
+        goXY(coord.x, coord.y);
+      }
+      goXY(path.get(0).x, path.get(0).y);
+      
+      if (curZ == z) {
+        break;
+      } else if (curZ + zStep > z) {
+        curZ = z;
+        if (stop) {
+          break;
+        } else {
+          stop = true;
+        }
+      } else {
+        curZ += zStep;
+      }
+    } while (true);
+
+    goUp();
   }
   
   // route a groove defined from (x1,y1) to (x2,y2) from start z to given z
@@ -173,7 +220,7 @@ public class MotorHolder {
   }
   
   // route a cutout from start z to given z
-  static void routeCutout(double cx, double cy, double cw, double ch, double startZ, double z) {
+  static void routeCutoutRect(double cx, double cy, double cw, double ch, double startZ, double z) {
     if (startZ >= z) return;
     if (cw <= 0 || ch <= 0) {
       throw new RuntimeException("Invalid dimension (" + cw + "x" + ch + ")");
@@ -235,7 +282,7 @@ public class MotorHolder {
       goZ(curZ);
       routePlane(cx, cy, cw, ch, planeDir);
       goZ(curZ-hoverDistance);
-      goXYRapid(cx-cw/2, cy-ch/2);
+      goXYRapid(cx-cw/2, cy-ch/2, false);
       
       planeDir = !planeDir;
       
@@ -348,7 +395,11 @@ public class MotorHolder {
   }
 
   static void goXYRapid(double x, double y) {
-    out.println("G00 Z" + hoverDistance);
+    goXYRapid(x, y, true);
+  }
+  
+  static void goXYRapid(double x, double y, boolean hover) {
+    if (hover) out.println("G00 Z" + hoverDistance);
     out.println("G00 X" + x + " Y" + y);
   }
   
@@ -366,5 +417,13 @@ public class MotorHolder {
     out.println("G00 X0 Y0");
     out.println("M05");
     out.println("M02");
+  }
+  
+  static class Coord {
+    double x; double y;
+    public Coord(double x, double y) {
+      this.x = x;
+      this.y = y;
+    }
   }
 }
